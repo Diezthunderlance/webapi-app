da9b4c921d7999bff522a9499ccd7f1d6f5b18fd
MatthewMerc-Wipro <Matthew.Mercado@wipro.com>
2019-03-27 15:08:12 -0500

    Added Nav Bar, Basic Form, services

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+// tslint:disable-next-line:quotemark
+import { BrowserModule } from "@angular/platform-browser";
+// tslint:disable-next-line:quotemark
+import { NgModule } from "@angular/core";
+// tslint:disable-next-line:quotemark
+import { HttpClientModule } from "@angular/common/http";
+// tslint:disable-next-line:quotemark
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+// tslint:disable-next-line:quotemark
+import { AppComponent } from "./app.component";
+// tslint:disable-next-line:quotemark
+import { ValueComponent } from "./value/value.component";
+// tslint:disable-next-line:quotemark
+import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpClientModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, ValueComponent, NavComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
