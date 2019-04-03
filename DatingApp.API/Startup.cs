c1133dc79f67fd86ee50f19b8e950f8a610d0c7c
MatthewMerc-Wipro <Matthew.Mercado@wipro.com>
2019-04-03 15:59:47 -0500

    Added client side photo upload

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,6 +36,7 @@ namespace DatingApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
+            services.Configure<CloudinarySettings>(Configuration.GetSection("CloudinarySettings"));
             services.AddAutoMapper();
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
