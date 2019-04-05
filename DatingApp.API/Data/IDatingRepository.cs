f2e8af7d95f619c5375e8bbcab0d63e699ae1bd1
MatthewMerc-Wipro <Matthew.Mercado@wipro.com>
2019-04-05 16:42:37 -0500

    Added message sending to API

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -13,8 +13,10 @@ namespace DatingApp.API.Data
         Task<PagedList<User>> GetUsers(UserParams UserParams);
         Task<User> GetUser(int id);
         Task<Photo> GetPhoto(int id);
-
         Task<Photo> GetMainPhotoForUser(int userId);
         Task<Like> GetLike(int userId, int recipientID);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<Message>> GetMessagesForUser();
+        Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientID);
     }
 }
